97c258ad02ed62279fd1b25f0e74d07926e113d1
vinod281 <57266038+vinod281@users.noreply.github.com>
2023-07-27 09:08:38 +0530

    GUI  change colour

---
+++ GUI/src/main/java/com/mycompany/gui/GUI.java
@@ -0,0 +1,12 @@
+package com.mycompany.gui;
+
+public class GUI 
+{
+
+    public static void main(String[] args) 
+    {
+        Gui_menu g1=new Gui_menu();
+        g1.setVisible(true);
+        
+    }
+}
