288d4504ffdee74036d967e7f83016c4d2efd92a
vinod281 <57266038+vinod281@users.noreply.github.com>
2023-07-27 09:09:42 +0530

    GUI-counts

---
+++ GUI_1/src/main/java/com/mycompany/gui_1/GUI_1.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.gui_1;
+
+
+public class GUI_1 
+{
+
+    public static void main(String[] args) 
+    {
+        ChangeCount cc1 =new ChangeCount();
+        cc1.setVisible(true);
+    }
+}
